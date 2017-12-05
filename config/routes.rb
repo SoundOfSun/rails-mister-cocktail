b9a5c63631ce1f1d975450578f510827b86b668d
Cecile Alfred <cecile.alfredx@gmail.com>
2017-12-05 12:38:18 +0000

    Made home page responsive on all devices

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#home"
-
   get "about", to: "pages#about"
 
   resources :cocktails, only: [ :index, :new, :show, :create, :edit, :update ] do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 # 2 rails modules
 require 'json'
 require 'open-uri'
 
-puts 'Seeding database...'
+p 'Cleaning database...'
+
+
+p 'Seeding database...'
 url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 text = open(url).read
 # .read extract the json file & accesses the text
@@ -20,4 +15,4 @@ ingredients = JSON.parse(text)
 ingredients["drinks"].each do |ingredient|
   Ingredient.create(name: ingredient["strIngredient1"])
 end
-puts 'Finished!'
+p 'Finished!'
