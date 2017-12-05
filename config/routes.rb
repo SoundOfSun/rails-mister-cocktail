3df2b61ab98a6e9b9d3e009e711e697ef7f25b92
Cecile Alfred <cecile.alfredx@gmail.com>
2017-12-05 00:46:10 +0000

    Added home + about pages, and icon + img for banner

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "cocktails#index"
+  root "pages#home"
+
+  get "about", to: "pages#about"
 
   resources :cocktails, only: [ :index, :new, :show, :create, :edit, :update ] do
     resources :doses, only: [:new, :create]
