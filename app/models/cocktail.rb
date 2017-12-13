75dbbc96624958dc4d6221caf5699dba11aa132e
Cecile Alfred <cecile.alfredx@gmail.com>
2017-12-13 12:51:41 +0000

    Created search method + conditional message on index page

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,8 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   has_attachment :photo
   validates :name, presence: true, uniqueness: true
+
+  def self.search(search)
+    where("name ILIKE ?", "%#{search}%")
+  end
 end
